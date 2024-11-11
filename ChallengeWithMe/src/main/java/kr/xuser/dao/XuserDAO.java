002bee3e98717162aef8bbeb725e328461733709
leee7 <leee7@LeeSuJong>
2024-11-11 15:22:52 +0900

    Xuser DAO VO

---
+++ ChallengeWithMe/src/main/java/kr/xuser/dao/XuserDAO.java
@@ -0,0 +1,95 @@
+package kr.xuser.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import kr.util.DBUtil;
+import kr.xuser.vo.XuserVO;
+
+public class XuserDAO {
+	// 싱글턴 패턴
+	private static XuserDAO instance = new XuserDAO();
+	
+	public static XuserDAO getInstance() {
+		return instance;
+	}
+	private XuserDAO() {};
+	
+	// 회원가입
+	public void insertUser(XuserVO xuser)throws Exception{
+		Connection con = null;
+		PreparedStatement ps = null;
+		PreparedStatement ps2 = null;
+		PreparedStatement ps3 = null;
+		ResultSet rs = null;
+		String sql = null;
+		long num = 0;
+		int i = 0;
+		
+		try {
+			con = DBUtil.getConnection();
+			con.setAutoCommit(false);
+			
+			// Xuser테이블 회원 번호 생성
+			sql = "SELECT xuser_seq.nextval FROM dual";
+			
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			if(rs.next()) {
+				num = rs.getLong(1);
+			}
+			// Xuser테이블 회원 번호 아이디 등록
+			sql = "INSERT INTO xuser (us_num, id) "
+					+ "VALUES(?, ?)";
+			
+			ps2 = con.prepareStatement(sql);
+			ps2.setLong(1, num);
+			ps2.setString(2, xuser.getId());
+			ps2.executeUpdate();
+			
+			// user_detail테이블 회원 개인정보 등록
+			sql = "INSERT INTO user_detail (us_num, us_name, us_birth, us_gen, us_tel, us_pw, us_nickname, us_email) "
+					+ "VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+			ps3 = con.prepareStatement(sql);
+			ps3.setLong(++i, num);
+			ps3.setString(++i, xuser.getName());
+			ps3.setString(++i, xuser.getBirth());
+			ps3.setInt(++i, xuser.getGen());
+			ps3.setString(++i, xuser.getTel());
+			ps3.setString(++i, xuser.getPasswd());
+			ps3.setString(++i, xuser.getNickname());
+			ps3.setString(++i, xuser.getEmail());
+			ps3.executeUpdate();
+			
+			con.commit();
+		} catch (Exception e) {
+			con.rollback();
+			throw new Exception(e);
+		} finally {
+			DBUtil.executeClose(null, ps3, null);
+			DBUtil.executeClose(null, ps2, null);
+			DBUtil.executeClose(rs, ps, con);
+		}
+	}
+	
+	// ID, 전화번호 중복체크 및 로그인 처리
+	public void checkUser(String id, String tel)throws Exception{
+		Connection con = null;
+		PreparedStatement ps = null;
+		String sql = null;
+		
+		try {
+			con = DBUtil.getConnection();
+			sql = "SELECT * FROM xuser LEFT OUTER JOIN "
+					+ "user_detail USING(user_num) WHERE id=? ";
+		} catch (Exception e) {
+			throw new Exception(e);
+		} finally {
+			DBUtil.executeClose(null, ps, con);
+		}
+		
+	}
+	
+	// 회원 상세 정보
+}
---
+++ ChallengeWithMe/src/main/java/kr/xuser/vo/XuserVO.java
@@ -0,0 +1,162 @@
+package kr.xuser.vo;
+
+public class XuserVO {
+	// 회원 정보
+	private long us_num;
+	private String id;
+	private int rank;
+	private int ban;
+	private String wallet;
+	private int admin;
+
+	// 회원 개인정보
+	private String name;
+	private String birth;
+	private int gen;
+	private String tel;
+	private String passwd;
+	private String nickname;
+	private String date;
+	private String rdate;
+	private String img;
+	private String email;
+
+	// 비밀번호 일치 여부 체크
+	public boolean isCheckedPassword(String userPasswd) {
+		// 회원상태(us_ban) : "0" ACTIVE "1" BAN "2" IDBAN
+		if (ban > 0 && passwd.equals(userPasswd)) {
+			return true;
+		}
+		return false;
+	}
+
+	// 회원 get set
+	public long getUs_num() {
+		return us_num;
+	}
+
+	public void setUs_num(long us_num) {
+		this.us_num = us_num;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	public int getBan() {
+		return ban;
+	}
+
+	public void setBan(int ban) {
+		this.ban = ban;
+	}
+
+	public String getWallet() {
+		return wallet;
+	}
+
+	public void setWallet(String wallet) {
+		this.wallet = wallet;
+	}
+
+	public int getAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(int admin) {
+		this.admin = admin;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+	public int getGen() {
+		return gen;
+	}
+
+	public void setGen(int gen) {
+		this.gen = gen;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getRdate() {
+		return rdate;
+	}
+
+	public void setRdate(String rdate) {
+		this.rdate = rdate;
+	}
+
+	public String getImg() {
+		return img;
+	}
+
+	public void setImg(String img) {
+		this.img = img;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
