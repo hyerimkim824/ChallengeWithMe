343f82e6a5f54c932cc470fe5970038ab3987431
Lukasoh <ekzmwor@gmail.com>
2024-12-02 21:07:13 +0900

    랭크 디자인 추가

--- ChallengeWithMe/src/main/java/kr/challenge/action/ChallengeGameConfirmAction.java
+++ ChallengeWithMe/src/main/java/kr/challenge/action/ChallengeGameConfirmAction.java
@@ -1,19 +1,18 @@
-package kr.game.action;
+package kr.challenge.action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import kr.controller.Action;
 
-public class GameConfirmAction implements Action{
+public class ChallengeGameConfirmAction implements Action{
 
 	@Override
 	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		
 		
 		
-		
 		return null;
 	}
-
+	
 }
--- ChallengeWithMe/src/main/java/kr/game/action/MathGame.java
+++
@@ -1,5 +0,0 @@
-package kr.game.action;
-
-public class MathGame {
-
-}
